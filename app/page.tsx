2668c8741e2e17e575c6ffa49468181cc2f18a5e
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-09-23 17:04:34 +0545

    prisma-setup,db.ts

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import { UserButton } from '@clerk/nextjs';
 export default function Home() {
   return (
     <>
-      <div className="flex justify-end">
-        <UserButton afterSignOutUrl="/" />
-      </div>
-      <div className="text-2xl text-red-500 font-semibold">
-        My Next-13 Portfolio
+      <div>
+        <div className="flex justify-end">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+        <p className="text-2xl text-red-500 font-semibold">
+          My Next-13 Portfolio
+        </p>
       </div>
     </>
   );
