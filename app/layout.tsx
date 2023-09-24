f00ca8223d52e3d9dfb309aa40faede82d432684
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-09-24 21:12:48 +0545

    middleware:private,public-routes,client-only,btn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Open_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'ANISH || PORTFOLIO',
@@ -16,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { HomePage } from '@/app/(main)/(routes)/home/home-page';
+import HomePage from './(main)/(routes)/home/page';
 
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="h-full">
+        {/* <div className="flex justify-end">
+          <UserButton afterSignOutUrl="/" />
+        </div> */}
+        <p className="text-2xl text-red-500 font-semibold">
+          My Next-13 Portfolio
+        </p>
         <HomePage />
       </div>
     </>
