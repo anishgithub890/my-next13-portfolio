bb20d2f91b275d4471511a4968fecbc8cfd15312
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-09-27 18:26:35 +0545

    nav-update,rm-container

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import Navbar from '@/components/navigation/navigation-navbar';
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full relative">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
-        <Sidebar />
-      </div>
-      <main className="md:pl-72 pb-10">
+      {/* <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900"> */}
+      {/* <Sidebar /> */}
+      {/* </div> */}
+      <main>
         <Navbar />
         <HomePage />
         {children}
@@ -16,3 +16,5 @@ export default function Home({ children }: { children: React.ReactNode }) {
     </div>
   );
 }
+
+//className="md:pl-72 pb-10"x
