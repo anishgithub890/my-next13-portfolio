781693309aded9a9bc700814f16ca3423be5425c
Anish Mahato <125879523+anishgithub890@users.noreply.github.com>
2023-10-06 17:54:33 +0545

    cloudinary-next-config/dialog.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
         premium:
           'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white border-0',
+        primary: 'bg-indigo-500 text-white hover:bg-indigo-500/90',
       },
       size: {
         default: 'h-10 px-4 py-2',
